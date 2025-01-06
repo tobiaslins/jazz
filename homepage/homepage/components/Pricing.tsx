a628f5cc971c9821c7baff571a18c5a0ba108dcb
Anselm <anselm.eickhoff@gmail.com>
2025-01-06 16:23:05 +0000

    Even simpler pricing

--- homepage/homepage/components/Pricing.tsx
+++ homepage/homepage/components/Pricing.tsx
@@ -11,6 +11,7 @@ import {
   LucideHammer,
   LucideHandshake,
   LucideIcon,
+  LucideInfinity,
   LucideMessageCircle,
   LucideMicroscope,
   LucidePhone,
@@ -80,38 +81,12 @@ export function Pricing() {
         <p className="text-sm">Everything you need for brand new projects.</p>
 
         <div>
-          <h4 className="text-sm font-semibold text-stone-900 dark:text-white">
-            Limits (lifted during public alpha)
-          </h4>
           <ul className="flex flex-col divide-y text-sm lg:text-base dark:divide-stone-900">
-            <ListItem icon={LucideUsers} variant="gray">
-              <s className="text-stone-500">max. 100 monthly active users</s>
+            <ListItem icon={LucideUsers}>
+              max. 100 monthly active users
             </ListItem>
-            <ListItem icon={LucideDatabase} variant="gray">
-              <s className="text-stone-500">max. 10 GB storage</s>
-            </ListItem>
-            <ListItem icon={LucideCloudDownload} variant="gray">
-              <s className="text-stone-500">max. 2 GB egress/mo</s>
-            </ListItem>
-          </ul>
-        </div>
-
-        <div>
-          <h4 className="text-sm font-semibold text-stone-900 dark:text-white">
-            Cloud features
-          </h4>
-          <ul className="flex flex-col divide-y text-sm lg:text-base dark:divide-stone-900">
-            <ListItem icon={LucideCheck}>Real-time sync</ListItem>
-            <ListItem icon={LucideDatabase}>Cloud storage</ListItem>
-          </ul>
-        </div>
-
-        <div>
-          <h4 className="text-sm font-semibold text-stone-900 dark:text-white">
-            Support
-          </h4>
-          <ul className="flex flex-col divide-y text-sm lg:text-base dark:divide-stone-900">
-            <ListItem icon={LucideMessageCircle}>Community support</ListItem>
+            <ListItem icon={LucideDatabase}>max. 10 GB storage</ListItem>
+            <ListItem icon={LucideCloudDownload}>max. 2 GB egress/mo</ListItem>
           </ul>
         </div>
 
@@ -128,7 +103,7 @@ export function Pricing() {
             </div>
             <div className="text-stone-900 dark:text-white">
               <span className="text-2xl font-light tabular-nums tracking-tighter">
-                $19
+                $4
               </span>
               <span className="text-sm font-normal text-stone-600 dark:text-stone-500">
                 /mo
@@ -142,49 +117,21 @@ export function Pricing() {
         </p>
 
         <div>
-          <h4 className="text-sm font-semibold text-stone-900 dark:text-white">
-            Limits & extra usage
-          </h4>
           <ul className="flex flex-col divide-y text-sm lg:text-base dark:divide-stone-900">
             <ListItem icon={LucideUsers}>
               max. 10,000 monthly active users
             </ListItem>
             <ListItem icon={LucideDatabase}>
-              incl. 500 GB storage{" "}
+              incl. 100 GB storage{" "}
               <span className="text-sm text-stone-900 dark:text-white">
                 (then $0.02 per GB)
               </span>
             </ListItem>
             <ListItem icon={LucideCloudDownload}>
-              incl. 100 GB egress/mo{" "}
+              incl. 20 GB egress/mo{" "}
               <span className="text-sm text-stone-900 dark:text-white">
                 (then $0.1 per GB)
               </span>
-            </ListItem>
-          </ul>
-        </div>
-
-        <div>
-          <h4 className="text-sm font-semibold text-stone-900 dark:text-white">
-            Cloud features
-          </h4>
-          <ul className="flex flex-col divide-y text-sm lg:text-base dark:divide-stone-900">
-            <ListItem icon={LucideZap}>Priority real-time sync</ListItem>
-            <ListItem icon={LucideDatabaseZap}>
-              SSD cloud storage & daily backups
-            </ListItem>
-            <ListItem icon={LucideSearch}>Basic user analytics</ListItem>
-          </ul>
-        </div>
-
-        <div>
-          <h4 className="text-sm font-semibold text-stone-900 dark:text-white">
-            Support
-          </h4>
-          <ul className="flex flex-col divide-y text-sm lg:text-base dark:divide-stone-900">
-            <ListItem icon={LucideMessageCircle}>Community support</ListItem>
-            <ListItem icon={LucideTicketCheck}>
-              Prioritised feature requests
             </ListItem>
           </ul>
         </div>
@@ -193,10 +140,7 @@ export function Pricing() {
           Coming soon
         </Button>
 
-        <p className="text-sm">
-          We&apos;ve designed this tier to be affordable but also
-          self-sustaining, so you can rely on it long-term.
-        </p>
+        <p className="text-sm">One month free trial.</p>
       </div>
       <div className="md:flex-1 flex flex-col gap-3 overflow-hidden rounded-xl p-6 shadow-lg shadow-gray-900/5 bg-white dark:bg-stone-925 pb-6">
         <h3 className="flex justify-between items-center font-semibold text-stone-900 text-xl dark:text-white">
@@ -220,49 +164,13 @@ export function Pricing() {
         </p>
 
         <div>
-          <h4 className="text-sm font-semibold text-stone-900 dark:text-white">
-            Limits
-          </h4>
           <ul className="flex flex-col divide-y text-sm lg:text-base dark:divide-stone-900">
-            <ListItem icon={LucideUsers}>Custom monthly active users</ListItem>
-            <ListItem icon={LucideDatabase}>Custom storage</ListItem>
-            <ListItem icon={LucideCloudDownload}>Custom egress</ListItem>
-          </ul>
-        </div>
-
-        <div>
-          <h4 className="text-sm font-semibold text-stone-900 dark:text-white">
-            Cloud features
-          </h4>
-          <ul className="flex flex-col divide-y text-sm lg:text-base dark:divide-stone-900">
-            <ListItem icon={LucideCrown}>
-              Highest-priority real-time sync
+            <ListItem icon={LucideInfinity}>
+              Custom users, storage & egress
             </ListItem>
-            <ListItem icon={LucideDatabaseZap}>
-              SSD cloud storage & custom backups
-            </ListItem>
-            <ListItem icon={LucideMicroscope}>Advanced user analytics</ListItem>
-          </ul>
-        </div>
-
-        <div>
-          <h4 className="text-sm font-semibold text-stone-900 dark:text-white">
-            Support
-          </h4>
-          <ul className="flex flex-col divide-y text-sm lg:text-base dark:divide-stone-900">
-            <ListItem icon={LucidePhone}>Dedicated support</ListItem>
-            <ListItem icon={LucideHammer}>Custom feature development</ListItem>
             <ListItem icon={LucideHandshake}>
               Rapid integration & premium onboarding
             </ListItem>
-          </ul>
-        </div>
-
-        <div>
-          <h4 className="text-sm font-semibold text-stone-900 dark:text-white">
-            Enterprise needs
-          </h4>
-          <ul className="flex flex-col divide-y text-sm lg:text-base dark:divide-stone-900">
             <ListItem icon={LucideBuilding2}>
               SLAs, on-prem & certifications
             </ListItem>
@@ -273,10 +181,7 @@ export function Pricing() {
           Book intro call
         </Button>
 
-        <p className="text-sm">
-          Our team of devs & product experts will get you going for free. Then
-          we&apos;ll make a deal just for you.
-        </p>
+        <p className="text-sm">Our expert team will get you going for free.</p>
       </div>
     </div>
   );
