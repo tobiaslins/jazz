aaf00b8a20fe5dcd3d0342754a9f6588e2c2c503
Guido D'Orsi <gu.dorsi@gmail.com>
2025-01-14 16:16:31 +0100

    feat: update starter example with anonymous auth

--- starters/react-demo-auth-tailwind/tests/starter.spec.ts
+++ starters/react-demo-auth-tailwind/tests/starter.spec.ts
@@ -1,15 +1,8 @@
 import { expect, test } from "@playwright/test";
-import { LoginPage } from "./pages/LoginPage";
 
 test("home page loads", async ({ page }) => {
-  const loginPage = new LoginPage(page);
-
-  await loginPage.goto();
-  await loginPage.fillUsername("Alice");
-  await loginPage.signup();
-
   await expect(page.getByText("Welcome!")).toBeVisible();
 
-  await page.getByLabel("First name").fill("Bob");
+  await page.getByLabel("Username").fill("Bob");
   await expect(page.getByText("Welcome, Bob!")).toBeVisible();
 });
