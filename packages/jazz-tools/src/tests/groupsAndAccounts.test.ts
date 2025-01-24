1227047b031456d9c135893f87230fa0414436fe
Guido D'Orsi <gu.dorsi@gmail.com>
2025-01-24 17:07:00 +0100

    fix: improve typescript types for group.addMember

--- packages/jazz-tools/src/tests/groupsAndAccounts.test.ts
+++ packages/jazz-tools/src/tests/groupsAndAccounts.test.ts
@@ -1,9 +1,16 @@
 import { WasmCrypto } from "cojson";
-import { describe, expect, test } from "vitest";
+import { beforeEach, describe, expect, test } from "vitest";
 import { Account, CoMap, Group, Profile, co } from "../exports.js";
+import { createJazzTestAccount } from "../testing.js";
 import { setupTwoNodes } from "./utils.js";
 
 const Crypto = await WasmCrypto.create();
+
+beforeEach(async () => {
+  await createJazzTestAccount({
+    isCurrentActiveAccount: true,
+  });
+});
 
 describe("Custom accounts and groups", async () => {
   class CustomProfile extends Profile {
@@ -179,5 +186,62 @@ describe("Group inheritance", () => {
     const loadedGroup = await serverNode.load(group._raw.id);
 
     expect(loadedGroup).not.toBe("unavailable");
+  });
+
+  test("everyone is valid only for reader and writer roles", () => {
+    const group = Group.create();
+    group.addMember("everyone", "reader");
+
+    expect(group.members).toContainEqual({
+      id: "everyone",
+      role: "reader",
+      account: undefined,
+      ref: undefined,
+    });
+
+    group.addMember("everyone", "writer");
+
+    expect(group.members).toContainEqual({
+      id: "everyone",
+      role: "writer",
+      account: undefined,
+      ref: undefined,
+    });
+
+    // @ts-expect-error - admin is not a valid role for everyone
+    expect(() => group.addMember("everyone", "admin")).toThrow();
+
+    expect(group.members).toContainEqual({
+      id: "everyone",
+      role: "writer",
+      account: undefined,
+      ref: undefined,
+    });
+
+    // @ts-expect-error - writeOnly is not a valid role for everyone
+    expect(() => group.addMember("everyone", "writeOnly")).toThrow();
+
+    expect(group.members).toContainEqual({
+      id: "everyone",
+      role: "writer",
+      account: undefined,
+      ref: undefined,
+    });
+  });
+
+  test("typescript should show an error when adding a member with a non-account role", async () => {
+    const account = await createJazzTestAccount({});
+
+    const group = Group.create();
+
+    // @ts-expect-error - Even though readerInvite is a valid role for an account, we don't allow it to not create confusion when using the intellisense
+    group.addMember(account, "readerInvite");
+
+    expect(group.members).toContainEqual(
+      expect.objectContaining({
+        id: account.id,
+        role: "readerInvite",
+      }),
+    );
   });
 });
