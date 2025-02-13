e0ef3fc1de8ad43fb81e65f956c2ff1c494d10e2
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2025-02-13 00:35:23 +0000

    Removes browser-integration from vite workspace
    Vite treats it as a src package, and that breaks turbo's test task resolution (running tests at the root level). Vitest doesn't need it to be there to run the tests.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   root: "./",
   test: {
-    workspace: ["packages/*", "tests/browser-integration"],
+    workspace: ["packages/*"],
     coverage: {
       enabled: false,
       provider: "istanbul",
