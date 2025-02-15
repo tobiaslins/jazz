510e8874e1d8c23ddf59a7c26902e465ed99e550
Guido D'Orsi <gu.dorsi@gmail.com>
2025-02-15 01:11:02 +0100

    Revert "perf: optimize queue processing under heavy load"
    This reverts commit b90393b43e3db17f7153cce7aba7c0daa6551227.

--- packages/cojson/src/PeerState.ts
+++ packages/cojson/src/PeerState.ts
@@ -93,7 +93,7 @@ export class PeerState {
 
     this.processing = true;
 
-    let entry: QueueEntry<SyncMessage> | undefined;
+    let entry: QueueEntry | undefined;
     while ((entry = this.queue.pull())) {
       // Awaiting the push to send one message at a time
       // This way when the peer is "under pressure" we can enqueue all
@@ -130,7 +130,7 @@ export class PeerState {
   }
 
   private closeQueue() {
-    let entry: QueueEntry<SyncMessage> | undefined;
+    let entry: QueueEntry | undefined;
     while ((entry = this.queue.pull())) {
       // Using resolve here to avoid unnecessary noise in the logs
       entry.resolve();
