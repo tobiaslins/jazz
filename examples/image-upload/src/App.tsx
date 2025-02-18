243ab074eb48a1e2f1bb9a6d8a70432f5b3ca832
Trisha Lim <hello@trishalim.com>
2025-02-18 15:34:41 +0700

    Remove non-functional nav from image upload example

--- examples/image-upload/src/App.tsx
+++ examples/image-upload/src/App.tsx
@@ -1,19 +1,8 @@
-import { useAccount } from "jazz-react";
 import ImageUpload from "./ImageUpload.tsx";
 
 function App() {
-  const { me, logOut } = useAccount();
-
   return (
     <>
-      <header>
-        <nav className="container">
-          <span>
-            You're logged in as <strong>{me?.profile?.name}</strong>
-          </span>
-          <button onClick={() => logOut()}>Log out</button>
-        </nav>
-      </header>
       <main className="container">
         <ImageUpload />
       </main>
