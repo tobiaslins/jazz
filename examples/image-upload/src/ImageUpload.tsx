f7b91b7ce169d976ff8dc5ce65f9bf83c21dcaa0
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2025-02-23 16:52:37 +0000

    Adds unload notification while uploading

--- examples/image-upload/src/ImageUpload.tsx
+++ examples/image-upload/src/ImageUpload.tsx
@@ -1,20 +1,48 @@
 import { createImage } from "jazz-browser-media-images";
 import { ProgressiveImg, useAccount } from "jazz-react";
 import { ImageDefinition } from "jazz-tools";
-import { ChangeEvent, useRef } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 function Image({ image }: { image: ImageDefinition }) {
+  const [isFullSize, setIsFullSize] = useState(false);
+
   return (
     <ProgressiveImg image={image}>
-      {({ src }) => <img src={src} />}
+      {({ src }) => (
+        <img
+          src={src}
+          onClick={() => setIsFullSize(!isFullSize)}
+          style={{
+            cursor: "pointer",
+            maxWidth: isFullSize ? "none" : "80vw",
+            maxHeight: isFullSize ? "none" : "80vh",
+            width: "auto",
+            height: "auto",
+            objectFit: "contain",
+          }}
+          title={isFullSize ? "Click to scale down" : "Click to show full size"}
+        />
+      )}
     </ProgressiveImg>
   );
 }
 
 export default function ImageUpload() {
   const { me } = useAccount();
-
+  const [isUploading, setIsUploading] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+      if (isUploading) {
+        e.preventDefault();
+        return "Upload in progress. Are you sure you want to leave?";
+      }
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => window.removeEventListener("beforeunload", handleBeforeUnload);
+  }, [isUploading]);
 
   const onImageChange = async (event: ChangeEvent<HTMLInputElement>) => {
     if (!me?.profile) return;
@@ -22,15 +50,19 @@ export default function ImageUpload() {
     const file = event.currentTarget.files?.[0];
 
     if (file) {
-      me.profile.image = await createImage(file, {
-        owner: me.profile._owner,
-      });
+      try {
+        setIsUploading(true);
+        me.profile.image = await createImage(file, {
+          owner: me.profile._owner,
+        });
+      } finally {
+        setIsUploading(false);
+      }
     }
   };
 
   const deleteImage = () => {
     if (!me?.profile) return;
-
     me.profile.image = null;
   };
 
@@ -38,7 +70,13 @@ export default function ImageUpload() {
     <>
       <div>{me?.profile?.image && <Image image={me.profile.image} />}</div>
 
-      <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "0.5rem",
+        }}
+      >
         {me?.profile?.image ? (
           <button type="button" onClick={deleteImage}>
             Delete image
@@ -49,9 +87,11 @@ export default function ImageUpload() {
             <input
               ref={inputRef}
               type="file"
-              accept="image/png, image/jpeg, image/gif"
+              accept="image/png, image/jpeg, image/gif, image/bmp"
               onChange={onImageChange}
+              disabled={isUploading}
             />
+            {isUploading && <span>Uploading...</span>}
           </div>
         )}
       </div>
