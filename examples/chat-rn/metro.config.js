abf65d5286ef302c84cf8da6cda5cb63ad3c1fb0
Brad Anderson <brad@sankatygroup.com>
2025-03-14 10:11:05 -0400

    try 0.78 on all RN apps

--- examples/chat-rn/metro.config.js
+++ examples/chat-rn/metro.config.js
@@ -1,12 +1,12 @@
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
-const blacklist = require("metro-config/src/defaults/exclusionList");
+// const blacklist = require("metro-config/src/defaults/exclusionList");
 const escape = require("escape-string-regexp");
 const path = require("path");
 
 // eslint-disable-next-line no-undef
 const projectRoot = __dirname;
 const workspaceRoot = path.resolve(projectRoot, "../..");
-const dupeDeps = ["react", "react-native"];
+// const dupeDeps = ["react", "react-native"];
 
 /**
  * Metro configuration
@@ -15,21 +15,22 @@ const dupeDeps = ["react", "react-native"];
  * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {
+  projectRoot,
   resolver: {
     // monorepo node_modules paths
     nodeModulesPaths: [
       path.resolve(projectRoot, "node_modules"),
       path.resolve(workspaceRoot, "node_modules"),
     ],
-    // ensure only one version is loaded for dupeDeps (not the one in projectRoot)
-    blacklistRE: blacklist(
-      dupeDeps.map(
-        (dep) =>
-          new RegExp(
-            `^${escape(path.join(projectRoot, "node_modules", dep))}$`,
-          ),
-      ),
-    ),
+    // // ensure only one version is loaded for dupeDeps (not the one in workspaceRoot)
+    // blacklistRE: blacklist(
+    //   dupeDeps.map(
+    //     (dep) =>
+    //       new RegExp(
+    //         `^${escape(path.join(workspaceRoot, "node_modules", dep))}$`,
+    //       ),
+    //   ),
+    // ),
     // extensions
     sourceExts: ["mjs", "js", "json", "ts", "tsx"],
   },
