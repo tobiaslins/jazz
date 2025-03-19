939e1d7a3c41ea7c36d6fadf65a413cdcfdd328f
James Vickery <14852491+jmsv@users.noreply.github.com>
2025-03-19 17:24:20 +0000

    fix rss urls and set canonical url

--- homepage/gcmp/app/api/rss/route.ts
+++ homepage/gcmp/app/api/rss/route.ts
@@ -1,20 +1,17 @@
 import { posts } from "@/lib/posts";
 import { Feed } from "feed";
 import { NextResponse } from "next/server";
+import { metaTags } from "@/app/layout";
 
 export async function GET() {
-  const baseUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "http://localhost:3000";
-
   const feed = new Feed({
     title: "Garden Computing Blog",
     description: "News from Garden Computing",
-    id: baseUrl,
-    link: baseUrl,
+    id: metaTags.url,
+    link: `${metaTags.url}/news`,
     language: "en",
-    image: `${baseUrl}/social-image.png`,
-    favicon: `${baseUrl}/favicon.ico`,
+    image: `${metaTags.url}/social-image.png`,
+    favicon: `${metaTags.url}/favicon.ico`,
     copyright: `${new Date().getFullYear()} Garden Computing, Inc.`,
   });
 
@@ -23,11 +20,11 @@ export async function GET() {
       title: post.meta.title,
       description: post.meta.subtitle,
       id: post.meta.slug,
-      link: `${baseUrl}/news/${post.meta.slug}`,
+      link: `${metaTags.url}/news/${post.meta.slug}`,
       date: new Date(post.meta.date),
       author: [{ name: post.meta.author.name }],
       guid: post.meta.slug,
-      image: `${baseUrl}${post.meta.coverImage}`,
+      image: `${metaTags.url}${post.meta.coverImage}`,
     });
   });
 
