73b120637c6880c138a76f1d95d7c12cf27c7264
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2025-03-20 14:44:41 +0000

    Add pointer tracking

--- examples/multi-cursors/src/Canvas.tsx
+++ examples/multi-cursors/src/Canvas.tsx
@@ -1,6 +1,14 @@
 function Canvas() {
+  const handleMouseMove = (e: React.MouseEvent<SVGSVGElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    console.log(x, y);
+  };
+
   return (
-    <svg width="100%" height="100%">
+    <svg width="100%" height="100%" onMouseMove={handleMouseMove}>
       <rect x="0" y="0" width="100%" height="100%" fill="white" />
     </svg>
   );
