5aad79005dc918ad39bc4cd3af2d3b1cf09b2594
James Vickery <14852491+jmsv@users.noreply.github.com>
2025-03-25 20:29:09 +0000

    rm tests

--- examples/multi-cursors/tests/starter.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("home page loads", async ({ page }) => {
-  await page.goto("/");
-  await expect(page.getByText("Welcome!")).toBeVisible();
-
-  await page.getByLabel("Name").fill("Bob");
-  await expect(page.getByText("Welcome, Bob!")).toBeVisible();
-});
