402008a08fc9b37b5386b5784f8c2cedae600247
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2025-03-28 09:23:15 +0000

    Drop console.logs from build

--- examples/multi-cursors/vite.config.ts
+++ examples/multi-cursors/vite.config.ts
@@ -4,4 +4,12 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: "esbuild",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+  },
 });
