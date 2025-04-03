31843b4b5e4ae25de085a3ad57b9e6d05481b8af
Brad Anderson <brad@sankatygroup.com>
2025-04-03 13:11:29 -0400

    feat: sqlite storage on by default

--- packages/jazz-expo/src/provider.tsx
+++ packages/jazz-expo/src/provider.tsx
@@ -1,14 +1,16 @@
 import { JazzProviderCore, JazzProviderProps } from "jazz-react-native-core";
 import React from "react";
 import { ExpoSecureStoreAdapter } from "./storage/expo-secure-store-adapter.js";
+import { ExpoSQLiteAdapter } from "./storage/expo-sqlite-adapter.js";
 
 export function JazzProvider(props: JazzProviderProps) {
   // Destructure kvStore and pass everything else via rest
-  const { kvStore, ...rest } = props;
+  const { kvStore, storage, ...rest } = props;
 
   return (
     <JazzProviderCore
       {...rest}
+      storage={storage ?? new ExpoSQLiteAdapter()}
       kvStore={kvStore ?? new ExpoSecureStoreAdapter()}
     />
   );
--- packages/jazz-react-native/src/provider.tsx
+++ packages/jazz-react-native/src/provider.tsx
@@ -1,10 +1,17 @@
 import { JazzProviderCore, JazzProviderProps } from "jazz-react-native-core";
 import React from "react";
 import { MMKVStore } from "./storage/mmkv-store-adapter.js";
+import { OPSQLiteAdapter } from "./storage/op-sqlite-adapter.js";
 
 export function JazzProvider(props: JazzProviderProps) {
   // Destructure kvStore and pass everything else via rest
-  const { kvStore, ...rest } = props;
+  const { kvStore, storage, ...rest } = props;
 
-  return <JazzProviderCore {...rest} kvStore={kvStore ?? new MMKVStore()} />;
+  return (
+    <JazzProviderCore
+      {...rest}
+      storage={storage ?? new OPSQLiteAdapter()}
+      kvStore={kvStore ?? new MMKVStore()}
+    />
+  );
 }
