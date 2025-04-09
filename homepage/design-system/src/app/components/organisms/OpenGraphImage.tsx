794c56dfac18febf7624d32d78d7941ff1b0b5fb
Sammii <kellow.sammii@gmail.com>
2025-04-09 09:22:45 +0100

    creating reusable OpenGraphImage component, updating styling

---
+++ homepage/design-system/src/app/components/organisms/OpenGraphImage.tsx
@@ -0,0 +1,48 @@
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
+import { JazzLogo } from "@components/atoms/logos/JazzLogo";
+import { ImageResponse } from "next/og";
+
+export const imageSize = {
+  width: 1200,
+  height: 630,
+};
+
+export const imageContentType = "image/png";
+
+export default async function OpenGraphImage({ title }: { title: string }) {
+  const manropeSemiBold = await readFile(
+    join(process.cwd(), "assets/fonts/Manrope-SemiBold.ttf"),
+  );
+
+  return new ImageResponse(
+    <div
+      style={{
+        fontSize: 111,
+        background: "white",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        padding: "77px",
+      }}
+    >
+      {title}
+      <div
+        style={{ display: "flex", position: "absolute", bottom: 10, right: 27 }}
+      >
+        <JazzLogo className="w-7" />
+      </div>
+    </div>,
+    {
+      ...imageSize,
+      fonts: [
+        {
+          name: "Manrope",
+          data: manropeSemiBold,
+        },
+      ],
+    },
+  );
+}
--- homepage/homepage/app/opengraph-image.tsx
+++ homepage/homepage/app/opengraph-image.tsx
@@ -1,51 +1,9 @@
-import { JazzLogo } from 'gcmp-design-system/src/app/components/atoms/logos/JazzLogo'
-import { ImageResponse } from 'next/og'
-import { readFile } from 'node:fs/promises'
-import { join } from 'node:path'
+import OpenGraphImage, { imageSize, imageContentType } from 'gcmp-design-system/src/app/components/organisms/OpenGraphImage';
 
-export const alt = 'Whip up an app'
-export const size = {
-  width: 1200,
-  height: 630,
-}
-
-export const contentType = 'image/png'
+export const title = 'Whip up an app'
+export const size = imageSize;
+export const contentType = imageContentType;
 
 export default async function Image() {
-  const manropeRegular = await readFile(
-    join(process.cwd(), 'assets/fonts/Manrope-Regular.ttf')
-  )
-
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 111,
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'flex-start',
-          padding: '77px',
-        }}
-      >
-        {alt}
-        <div style={{ display: 'flex', position: 'absolute', bottom: 10, right: 27 }}>
-          <JazzLogo className="w-15" />
-        </div>
-      </div>
-    ),
-    {
-      ...size,
-      fonts: [
-        {
-          name: 'Manrope',
-          data: manropeRegular,
-          style: 'normal',
-          weight: 400,
-        },
-      ],
-    }
-  )
+  return OpenGraphImage({ title })
 }
