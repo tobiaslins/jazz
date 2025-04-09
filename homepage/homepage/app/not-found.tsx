09b59ed18bf85f20d711ee4c66b2e6588943453e
Sammii <kellow.sammii@gmail.com>
2025-04-09 17:07:18 +0100

    amending styling and code tidy

--- homepage/homepage/app/not-found.tsx
+++ homepage/homepage/app/not-found.tsx
@@ -1,14 +1,13 @@
-import Link from 'next/link'
 import { Icon404 } from 'gcmp-design-system/src/app/components/atoms/icons/404'
  
 export default function NotFound() {
   const text = "Don't Worry 'Bout Me";
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-flex-start justify-center min-h-screen w-auto">
       <Icon404 className="w-75" />
-      <h1 className="text-3xl font-semibold m-7">{text}</h1>
-      <p className="m-3">Page not found.</p>
+      <h1 className="text-3xl font-semibold my-7">{text}</h1>
+      <p>Page not found.</p>
     </div>
   )
 }
