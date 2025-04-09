197317efbf9c5da3285fc255a333c3acfed3b7f0
Sammii <kellow.sammii@gmail.com>
2025-04-09 13:28:15 +0100

    amend width & height defaults on logo

--- homepage/design-system/src/app/components/atoms/logos/JazzLogo.tsx
+++ homepage/design-system/src/app/components/atoms/logos/JazzLogo.tsx
@@ -2,8 +2,8 @@ import { clsx } from "clsx";
 
 export function JazzLogo({
   className,
-  width = 386,
-  height = 146,
+  width = undefined,
+  height = undefined,
 }: { className?: string; width?: number; height?: number }) {
   return (
     <svg
