9b2de387ed3e0f34e27aee1320f86d033a778d05
Sammii <kellow.sammii@gmail.com>
2025-04-09 17:22:26 +0100

    updating styling

--- homepage/homepage/app/not-found.tsx
+++ homepage/homepage/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   const text = "Don't Worry 'Bout Me";
 
   return (
-    <div className="flex flex-col items-flex-start justify-center min-h-screen w-auto">
+    <div className="flex flex-col items-flex-start justify-center min-h-[95vh] w-auto pb-32">
       <Icon404 className="w-75" />
       <h1 className="text-3xl font-semibold my-7">{text}</h1>
       <p>Page not found.</p>
