4f5e4716678fffd40e8b7c8c008728b90f6d6c81
Sammii <kellow.sammii@gmail.com>
2025-04-09 10:44:42 +0100

    adding alt and seeing if anything affects this svg logo

--- homepage/homepage/app/opengraph-image.tsx
+++ homepage/homepage/app/opengraph-image.tsx
@@ -3,6 +3,7 @@ import OpenGraphImage, { imageSize, imageContentType } from 'gcmp-design-system/
 export const title = 'Whip up an app'
 export const size = imageSize;
 export const contentType = imageContentType;
+export const alt = title;
 
 export default async function Image() {
   return OpenGraphImage({ title })
