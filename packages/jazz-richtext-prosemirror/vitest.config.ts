5118ff658596207dcf6b19fd1c18b064c85a601b
Guido D'Orsi <gu.dorsi@gmail.com>
2025-05-12 14:34:32 +0200

    chore: remove unused typecheck clause

--- packages/jazz-richtext-prosemirror/vitest.config.ts
+++ packages/jazz-richtext-prosemirror/vitest.config.ts
@@ -3,10 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom",
-    typecheck: {
-      enabled: true,
-      checker: "tsc",
-    },
     globals: false,
   },
 });
