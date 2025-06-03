4b908e3024d23e3f3c780648a84f824d8e0fdc54
Sammii <kellow.sammii@gmail.com>
2025-06-03 15:15:03 +0100

    refactor Forms view into own component

---
+++ homepage/design-system/src/app/components/Forms.tsx
@@ -0,0 +1,15 @@
+import { NewsletterForm } from "@/components/organisms/NewsletterForm";
+
+export function Forms() {
+  return (
+    <div>
+      <h2 id="forms" className="text-xl mt-5 mb-2 font-bold">
+        Forms
+      </h2>
+
+      <div className="p-3 border">
+        <NewsletterForm />
+      </div>
+    </div>
+  );
+}
