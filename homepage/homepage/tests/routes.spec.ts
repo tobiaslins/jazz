d555b18c11ee7ed612e86afc132d76e9ba49e24e
Trisha Lim <hello@trishalim.com>
2025-06-03 09:25:28 +0100

    test homepage routes (#2399)
    * test homepage routes
    
    * add playwright.yml to homepage dir
    
    * Revert "add playwright.yml to homepage dir"
    
    This reverts commit f0507ee4f7dd29d25db56d3227937c901f9225a0.
    
    * Add turbo dep to homepage
    
    * Split homepage playwright script out
    
    * remove playwright from test command
    
    * install homepage deps
    
    * update lock file
    
    ---------
    
    Co-authored-by: Benjamin S. Leveritt <benjamin@leveritt.co.uk>

---
+++ homepage/homepage/tests/routes.spec.ts
@@ -0,0 +1,54 @@
+import { expect, test } from "@playwright/test";
+
+test.describe("Top-level pages load", () => {
+  test("homepage loads", async ({ page }) => {
+    const response = await page.goto("/");
+    expect(response?.ok()).toBeTruthy();
+  });
+
+  test("examples page loads", async ({ page }) => {
+    const response = await page.goto("/examples");
+    expect(response?.ok()).toBeTruthy();
+  });
+
+  test("showcase page loads", async ({ page }) => {
+    const response = await page.goto("/showcase");
+    expect(response?.ok()).toBeTruthy();
+  });
+
+  test("cloud page loads", async ({ page }) => {
+    const response = await page.goto("/cloud");
+    expect(response?.ok()).toBeTruthy();
+  });
+
+  test("status page loads", async ({ page }) => {
+    const response = await page.goto("/status");
+    expect(response?.ok()).toBeTruthy();
+  });
+});
+
+test.describe("Docs pages load", () => {
+  test("docs intro loads", async ({ page }) => {
+    const response = await page.goto("/docs");
+    expect(response?.ok()).toBeTruthy();
+  });
+
+  [
+    "react",
+    "react-native",
+    "react-native-expo",
+    "vue",
+    "svelte",
+    "vanilla",
+  ].forEach((framework) => {
+    test(`docs for ${framework} loads`, async ({ page }) => {
+      const response = await page.goto(`/docs/${framework}`);
+      expect(response?.ok()).toBeTruthy();
+    });
+  });
+
+  test("/docs redirects to /docs/react", async ({ page }) => {
+    await page.goto("/docs");
+    await expect(page).toHaveURL(/\/docs\/react$/);
+  });
+});
