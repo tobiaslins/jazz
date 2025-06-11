99d1f3f28b4c50a5df392c7628b98907c57ad053
Sammii <kellow.sammii@gmail.com>
2025-06-11 15:13:22 +0100

    add toast refacvtor table with new isCopyable prop + functionality

--- homepage/design-system/src/components/molecules/Table.tsx
+++ homepage/design-system/src/components/molecules/Table.tsx
@@ -1,16 +1,24 @@
+"use client";
 import { clsx } from "clsx";
+import { toast } from "react-hot-toast";
 
 export interface TableProps extends React.HTMLAttributes<HTMLTableElement> {
   className?: string;
   tableData: {
     headers: string[];
     data: {
-      [key: string]: string;
+      [key: string]: string | string[];
     }[];
   };
+  copyable?: boolean;
 }
 
-export function Table({ className, tableData, ...tableProps }: TableProps) {
+export function Table({
+  className,
+  tableData,
+  copyable,
+  ...tableProps
+}: TableProps) {
   return (
     <table
       className={clsx("w-full border border-gray-200 rounded-lg", className)}
@@ -28,7 +36,7 @@ export function Table({ className, tableData, ...tableProps }: TableProps) {
       <tbody className="border-t border-gray-200">
         {tableData.data.map((row, index) => (
           <tr
-            key={row.id}
+            key={row.id as string}
             className={clsx(
               index % 2 === 0
                 ? "bg-gray-100 hover:bg-gray-200/50"
@@ -37,8 +45,26 @@ export function Table({ className, tableData, ...tableProps }: TableProps) {
             )}
           >
             {tableData.headers.map((header, index) => (
-              <td key={header} className="pl-1">
-                {row[header]}
+              <td
+                key={header}
+                className={clsx(
+                  index === 0 && "pl-1",
+                  typeof row[header] !== "string" && "flex",
+                )}
+              >
+                {typeof row[header] !== "string" ? (
+                  <TableDataContainer isCopyable={copyable}>
+                    {row[header].map((item) => (
+                      <div className="hover:underline" key={item}>
+                        {item}
+                      </div>
+                    ))}
+                  </TableDataContainer>
+                ) : (
+                  <TableDataContainer isCopyable={copyable}>
+                    {row[header as keyof typeof row]}
+                  </TableDataContainer>
+                )}
               </td>
             ))}
           </tr>
@@ -47,3 +73,23 @@ export function Table({ className, tableData, ...tableProps }: TableProps) {
     </table>
   );
 }
+
+const TableDataContainer = ({
+  children,
+  className,
+  isCopyable,
+}: { children: React.ReactNode; className?: string; isCopyable?: boolean }) => {
+  return (
+    <div
+      className={clsx("flex gap-2", className, isCopyable && "cursor-pointer")}
+      onClick={() => {
+        if (isCopyable && children) {
+          navigator.clipboard.writeText(children.toString());
+          toast.success("Copied to clipboard");
+        }
+      }}
+    >
+      {children}
+    </div>
+  );
+};
