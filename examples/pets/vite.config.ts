57e748d2bac4db66831fd491de63623dc34f42ca
Trisha Lim <hello@trishalim.com>
2025-06-13 13:31:54 +0100

    undo upgrading pets and music app

--- examples/pets/vite.config.ts
+++ examples/pets/vite.config.ts
@@ -1,11 +1,11 @@
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), topLevelAwait()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
