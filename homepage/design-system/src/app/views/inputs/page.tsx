7605d228f217e76e790283e450c4e7b5a1ddb723
Sammii <kellow.sammii@gmail.com>
2025-06-17 16:10:36 +0100

    page update inputs

--- homepage/design-system/src/app/views/inputs/page.tsx
+++ homepage/design-system/src/app/views/inputs/page.tsx
@@ -16,23 +16,6 @@ export default function InputsView() {
   return (
     <ViewsLayout>
       <div className="p-3">
-        <div className="pb-4 flex gap-6 flex-col md:flex-row">
-          <h3 className="text-md font-semibold">Switches</h3>
-          <Switch
-            label="Switch default (md) (Primary)"
-            id="switch-md"
-            checked={checked.md}
-            onChange={() => setChecked({ ...checked, md: !checked.md })}
-          />
-          <Switch
-            label="Switch (sm) success"
-            id="switch-sm"
-            checked={checked.sm}
-            onChange={() => setChecked({ ...checked, sm: !checked.sm })}
-            size="sm"
-            variant="success"
-          />
-        </div>
         <h3 className="text-md font-semibold mb-2">Inputs</h3>
         <div className="flex flex-col gap-2">
           <Input
