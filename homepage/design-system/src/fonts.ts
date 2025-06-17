7e605bb1f34cb57e21250ad910afe628f2095dc9
Trisha Lim <hello@trishalim.com>
2025-06-17 18:24:42 +0100

    save fonts locally

--- homepage/design-system/src/fonts.ts
+++ homepage/design-system/src/fonts.ts
@@ -1,15 +1,75 @@
-import { Manrope } from "next/font/google";
-import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 
-const manrope = Manrope({
-  subsets: ["latin"],
+const manrope = localFont({
+  src: [
+    {
+      path: "../fonts/Manrope-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Manrope-Bold.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Manrope-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Manrope-Medium.woff",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Manrope-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Manrope-SemiBold.woff",
+      weight: "600",
+      style: "normal",
+    },
+  ],
   variable: "--font-manrope",
   display: "swap",
 });
 
-const inter = Inter({
-  subsets: ["latin"],
+const inter = localFont({
+  src: [
+    {
+      path: "../fonts/Inter-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Inter-Medium.woff",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Inter-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Inter-Regular.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Inter-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Inter-SemiBold.woff",
+      weight: "600",
+      style: "normal",
+    },
+  ],
   variable: "--font-inter",
   display: "swap",
 });
