a1d31566edfa42196bfcb08f97697bf122339172
Sammii <kellow.sammii@gmail.com>
2025-06-19 11:34:50 +0100

    adding views layout to landing page to look like views routes

--- homepage/design-system/src/app/page.tsx
+++ homepage/design-system/src/app/page.tsx
@@ -1,10 +1,14 @@
+import { ViewsLayout } from "./views/ViewsLayout";
 import Colors from "./views/colors/page";
 
 export default function Home() {
   return (
     <main>
       <div className="col-span-8 overflow-y-scroll">
-        <Colors />
+        <ViewsLayout>
+          <h1 className="text-2xl font-bold mt-4">Colors</h1>
+          <Colors />
+        </ViewsLayout>
       </div>
     </main>
   );
