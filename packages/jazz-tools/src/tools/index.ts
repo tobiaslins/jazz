65e78014fabcda26e432ddea75db9d7c3e97acfa
Guido D'Orsi <gu.dorsi@gmail.com>
2025-06-19 18:38:48 +0200

    feat: export logger from jazz-tools

--- packages/jazz-tools/src/tools/index.ts
+++ packages/jazz-tools/src/tools/index.ts
@@ -1,3 +1,8 @@
 export * from "./exports.js";
 
-export { MAX_RECOMMENDED_TX_SIZE, cojsonInternals } from "cojson";
+export {
+  MAX_RECOMMENDED_TX_SIZE,
+  cojsonInternals,
+  logger,
+  LogLevel,
+} from "cojson";
