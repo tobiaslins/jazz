afae2649f52abae342edd986e681f3d168f4cae9
Matteo Manchi <matteo.manchi@gmail.com>
2025-07-28 11:49:07 +0200

    docs: new docs for Image Management

--- homepage/homepage/content/docs/docNavigationItems.js
+++ homepage/homepage/content/docs/docNavigationItems.js
@@ -230,6 +230,7 @@ export const docNavigationItems = [
           "react-native": 100,
           "react-native-expo": 100,
           vanilla: 100,
+          svelte: 100,
         },
       },
       {
