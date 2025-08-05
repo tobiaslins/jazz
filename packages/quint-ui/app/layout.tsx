d190097ed97286aac6ad7e6d3079c77ac6b054cf
Sammii <kellow.sammii@gmail.com>
2025-08-05 10:55:16 +0100

    creating tempory nav

--- packages/quint-ui/app/layout.tsx
+++ packages/quint-ui/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/src/globals.css";
+import Link from "next/link";
 
 export default function RootLayout({
   children,
@@ -11,6 +12,11 @@ export default function RootLayout({
         <title>Quint UI</title>
       </head>
       <body>
+        <div className="flex gap-4 text-sm bg-stone-100 dark:bg-stone-800 p-2 px-5 rounded-md font-semibold">
+          <Link href="/">Colors</Link>
+          <Link href="/docs/button">Button</Link>
+          <Link href="/docs/icons">Icons</Link>
+        </div>
         <div className="container m-21">{children}</div>
       </body>
     </html>
