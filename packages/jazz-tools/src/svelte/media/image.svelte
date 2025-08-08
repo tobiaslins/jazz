6c35d0031d1ce9bed34994688573e1267860012e
Matteo Manchi <matteo.manchi@gmail.com>
2025-08-08 13:47:24 +0200

    chore(jazz-tools/svelte): refactor image component + svelte testing

--- packages/jazz-tools/src/svelte/media/image.svelte
+++ packages/jazz-tools/src/svelte/media/image.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { type FileStream, ImageDefinition } from "jazz-tools";
+import { ImageDefinition } from "jazz-tools";
 import { highestResAvailable } from "jazz-tools/media";
 import { onDestroy } from "svelte";
 import type { HTMLImgAttributes } from "svelte/elements";
@@ -13,66 +13,119 @@ interface ImageProps extends Omit<HTMLImgAttributes, "width" | "height"> {
 
 const { imageId, width, height, ...rest }: ImageProps = $props();
 
-// Create reactive state for the image
 const imageState = new CoState(ImageDefinition, imageId);
+let lastBestImage: [string, string] | null = null;
 
-let src: string | undefined = $state(imageState.current?.placeholderDataURL);
+/**
+ * For lazy loading, we use the browser's strategy for images with loading="lazy".
+ * We use an empty image, and when the browser triggers the load event, we load the best available image.
+ * On page loading, if the image url is already in browser's cache, the load event is triggered immediately.
+ * This is why we need to use a different blob url for every image.
+ */
+let waitingLazyLoading = $state(rest.loading === "lazy");
+const lazyPlaceholder = $derived.by(() =>
+  waitingLazyLoading ? URL.createObjectURL(emptyPixelBlob) : undefined,
+);
 
 const dimensions = $derived.by<{
   width: number | undefined;
   height: number | undefined;
 }>(() => {
-  const w =
-    width === "original" ? imageState.current?.originalSize?.[0] : width;
-  const h =
-    height === "original" ? imageState.current?.originalSize?.[1] : height;
+  const originalWidth = imageState.current?.originalSize?.[0];
+  const originalHeight = imageState.current?.originalSize?.[1];
 
-  return { width: w, height: h };
+  // Both width and height are "original"
+  if (width === "original" && height === "original") {
+    return { width: originalWidth, height: originalHeight };
+  }
+
+  // Width is "original", height is a number
+  if (width === "original" && typeof height === "number") {
+    if (originalWidth && originalHeight) {
+      return {
+        width: Math.round((height * originalWidth) / originalHeight),
+        height,
+      };
+    }
+    return { width: undefined, height };
+  }
+
+  // Height is "original", width is a number
+  if (height === "original" && typeof width === "number") {
+    if (originalWidth && originalHeight) {
+      return {
+        width,
+        height: Math.round((width * originalHeight) / originalWidth),
+      };
+    }
+    return { width, height: undefined };
+  }
+
+  // In all other cases, use the property value:
+  return {
+    width: width === "original" ? originalWidth : width,
+    height: height === "original" ? originalHeight : height,
+  };
 });
 
-$effect(() => {
+const src = $derived.by(() => {
+  if (waitingLazyLoading) {
+    return lazyPlaceholder;
+  }
+
   const image = imageState.current;
-  if (!image) return;
+  if (!image) return undefined;
 
-  src = image.placeholderDataURL;
-  let lastBestImage: FileStream | null = null;
+  const bestImage = highestResAvailable(
+    image,
+    dimensions.width || dimensions.height || 9999,
+    dimensions.height || dimensions.width || 9999,
+  );
 
-  const unsub = image.subscribe({}, (update) => {
-    const bestImage = highestResAvailable(
-      update,
-      dimensions.width || dimensions.height || 9999,
-      dimensions.height || dimensions.width || 9999,
-    );
+  if (!bestImage) return image.placeholderDataURL;
+  if (lastBestImage?.[0] === bestImage.image.id) return lastBestImage?.[1];
 
-    if (!bestImage) return;
+  const blob = bestImage.image.toBlob();
 
-    if (lastBestImage === bestImage.image) return;
+  if (blob) {
+    const url = URL.createObjectURL(blob);
+    revokeObjectURL(lastBestImage?.[1]);
+    lastBestImage = [bestImage.image.id, url];
+    return url;
+  }
 
-    const blob = bestImage.image.toBlob();
-
-    if (blob) {
-      if (src) URL.revokeObjectURL(src);
-      src = URL.createObjectURL(blob);
-      lastBestImage = bestImage.image;
-    }
-  });
-
-  return unsub;
+  return image.placeholderDataURL;
 });
 
 // Cleanup object URL on component destroy
 onDestroy(() => {
-  if (src) {
-    URL.revokeObjectURL(src);
-  }
+  revokeObjectURL(lastBestImage?.[1]);
 });
+
+function revokeObjectURL(url: string | undefined) {
+  if (url && url.startsWith("blob:")) {
+    URL.revokeObjectURL(url);
+  }
+}
+
+const emptyPixelBlob = new Blob(
+  [
+    Uint8Array.from(
+      atob(
+        "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8/5+hHgAHggJ/PchI7wAAAABJRU5ErkJggg==",
+      ),
+      (c) => c.charCodeAt(0),
+    ),
+  ],
+  { type: "image/png" },
+);
 </script>
 
-{#if imageState.current}
-  <img
-    {src}
-    width={dimensions.width}
-    height={dimensions.height}
-    {...rest}
-  />
-{/if}
+<img
+  {src}
+  width={dimensions.width}
+  height={dimensions.height}
+  alt={rest.alt}
+  onload={() => {waitingLazyLoading = false}}
+  {...rest}
+/>
--- packages/jazz-tools/vitest.config.ts
+++ packages/jazz-tools/vitest.config.ts
@@ -1,6 +1,19 @@
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { svelteTesting } from "@testing-library/svelte/vite";
 import { defineProject } from "vitest/config";
 
 export default defineProject({
+  plugins: [
+    svelte(),
+    svelteTesting({
+      resolveBrowser: false,
+    }),
+  ],
+  resolve: {
+    // 'browser' for Svelte Testing Library
+    // 'node' for "msw/node"
+    conditions: ["browser", "node"],
+  },
   test: {
     name: "jazz-tools",
     include: ["src/**/*.test.{js,ts,tsx,svelte}"],
