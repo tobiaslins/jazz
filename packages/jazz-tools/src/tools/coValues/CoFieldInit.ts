a925194da7263796e9481089fb18dc462e27674f
NicoR <nmrainhart@gmail.com>
2025-08-18 16:36:24 -0300

    feat: show better type hints for `CoFieldInit` inputs

--- packages/jazz-tools/src/tools/coValues/CoFieldInit.ts
+++ packages/jazz-tools/src/tools/coValues/CoFieldInit.ts
@@ -3,7 +3,6 @@ import { CoList } from "./coList";
 import { CoMap, CoMapInit } from "./coMap";
 import { CoPlainText } from "./coPlainText";
 import { CoRichText } from "./coRichText";
-import { CoValue } from "./interfaces";
 
 /**
  * Returns the type of values that can be used to initialize a field of the provided type.
@@ -11,14 +10,11 @@ import { CoValue } from "./interfaces";
  * For CoValue references, either a CoValue of the same type, or a plain JSON value that can be
  * converted to the CoValue type are allowed.
  */
-export type CoFieldInit<V> =
-  | V
-  | (V extends CoValue
-      ? V extends CoMap
-        ? CoMapInit<V>
-        : V extends CoList<infer T> | CoFeed<infer T>
-          ? ReadonlyArray<CoFieldInit<T>>
-          : V extends CoPlainText | CoRichText
-            ? string
-            : never
-      : never);
+// Note: we don't define this type as V | ... because it prevents TS from inlining the type
+export type CoFieldInit<V> = V extends CoMap
+  ? V | CoMapInit<V>
+  : V extends CoList<infer T> | CoFeed<infer T>
+    ? V | ReadonlyArray<CoFieldInit<T>>
+    : V extends CoPlainText | CoRichText
+      ? V | string
+      : V;
