dfa7fe7ca5a00d0960a423229d81427d9925d950
Guido D'Orsi <gu.dorsi@gmail.com>
2025-09-23 13:07:25 +0200

    feat: add SyncConnectionStatus class on svelte

--- tests/jazz-svelte/src/routes/+page.svelte
+++ tests/jazz-svelte/src/routes/+page.svelte
@@ -1,3 +1,4 @@
 <a href="/costate">CoState</a>
 <a href="/media">Media</a>
 <a href="/virtual-list">Virtual List</a>
+<a href="/connection-status">Connection Status</a>
---
+++ tests/jazz-svelte/src/routes/connection-status/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { SyncConnectionStatus } from "jazz-tools/svelte";
+
+  const connectionStatus = new SyncConnectionStatus();
+</script>
+
+<div>
+  <h1>Connection Status</h1>
+  <p data-testid="connected">{connectionStatus.current ? "true" : "false"}</p>
+</div>
