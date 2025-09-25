0dcb66d9d21c84152dff5b9a798ae714b1c57cde
Youssef Gaber <1728215+Gabrola@users.noreply.github.com>
2025-09-25 08:04:49 +0300

    more react benchmarking

--- packages/jazz-tools/src/react-core/tests/subscription.bench.tsx
+++ packages/jazz-tools/src/react-core/tests/subscription.bench.tsx
@@ -1,7 +1,7 @@
 // @vitest-environment happy-dom
 
 import { cojsonInternals } from "cojson";
-import { co } from "jazz-tools";
+import { co, z } from "jazz-tools";
 import { describe, bench } from "vitest";
 import {
   useAccountSubscription,
@@ -90,5 +90,230 @@ describe("1000 value loads", () => {
       render(<UseAccountWithSelector />);
     },
     { iterations: 50 },
+  );
+});
+
+describe("deeply resolved coMaps", async () => {
+  const Task = co.map({
+    title: z.string(),
+    get project() {
+      return Project;
+    },
+  });
+
+  const TaskList = co.list(Task);
+
+  const Project = co.map({
+    name: z.string(),
+    tasks: TaskList,
+    draftTasks: TaskList,
+    deletedTasks: TaskList,
+  });
+
+  const Organization = co.map({
+    name: z.string(),
+    projects: co.list(Project),
+  });
+
+  const AccountRoot = co.map({
+    organizations: co.list(Organization),
+  });
+
+  const AccountSchema = co
+    .account({
+      root: AccountRoot,
+      profile: co.profile(),
+    })
+    .withMigration(async (account) => {
+      if (!account.$jazz.has("root")) {
+        account.$jazz.set("root", {
+          organizations: [
+            {
+              name: "My organization",
+              projects: [
+                {
+                  name: "My project",
+                  tasks: [],
+                  draftTasks: [],
+                  deletedTasks: [],
+                },
+              ],
+            },
+          ],
+        });
+      }
+    });
+
+  const account = await createJazzTestAccount({
+    AccountSchema,
+    isCurrentActiveAccount: true,
+    creationProps: { name: "Hermes Puggington" },
+  });
+
+  const root = await account.root.$jazz.ensureLoaded({
+    resolve: {
+      organizations: {
+        $each: {
+          projects: {
+            $each: true,
+          },
+        },
+      },
+    },
+  });
+
+  const project = root.organizations[0]?.projects[0]!;
+
+  for (let i = 0; i < 250; i++) {
+    const taskList = (["tasks", "draftTasks", "deletedTasks"] as const)[i % 3]!;
+
+    project[taskList].$jazz.push({
+      title: `Task ${i}`,
+      project,
+    });
+  }
+
+  const SingleSubscriptionTasks = ({
+    subscription,
+    taskListType,
+  }: {
+    subscription: CoValueSubscription<
+      typeof AccountSchema,
+      {
+        root: {
+          organizations: {
+            $each: {
+              projects: {
+                $each: {
+                  tasks: { $each: { project: true } };
+                  draftTasks: { $each: { project: true } };
+                  deletedTasks: { $each: { project: true } };
+                };
+              };
+            };
+          };
+        };
+      }
+    >;
+    taskListType: "tasks" | "draftTasks" | "deletedTasks";
+  }) => {
+    const allProjectsTasks = useSubscriptionSelector(subscription, {
+      select: (account) => {
+        return account?.root.organizations.flatMap((org) =>
+          org?.projects.flatMap((project) =>
+            project?.[taskListType]?.flatMap((task) => task),
+          ),
+        );
+      },
+    });
+
+    return (
+      <div>
+        {allProjectsTasks?.map((task) => (
+          <div key={task?.$jazz.id}>{task?.title}</div>
+        ))}
+      </div>
+    );
+  };
+
+  const SingleSubscription = () => {
+    const subscription = useAccountSubscription(AccountSchema, {
+      resolve: {
+        root: {
+          organizations: {
+            $each: {
+              projects: {
+                $each: {
+                  tasks: { $each: { project: true } },
+                  draftTasks: { $each: { project: true } },
+                  deletedTasks: { $each: { project: true } },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return (
+      <div>
+        <SingleSubscriptionTasks
+          subscription={subscription}
+          taskListType="tasks"
+        />
+        <SingleSubscriptionTasks
+          subscription={subscription}
+          taskListType="draftTasks"
+        />
+        <SingleSubscriptionTasks
+          subscription={subscription}
+          taskListType="deletedTasks"
+        />
+      </div>
+    );
+  };
+
+  bench(
+    "useAccountSubscription + useSubscriptionSelector",
+    () => {
+      render(<SingleSubscription />);
+    },
+    { iterations: 200 },
+  );
+
+  const MultipleSubscriptionTasks = ({
+    taskListType,
+  }: {
+    taskListType: "tasks" | "draftTasks" | "deletedTasks";
+  }) => {
+    const subscription = useAccountWithSelector(AccountSchema, {
+      resolve: {
+        root: {
+          organizations: {
+            $each: {
+              projects: {
+                $each: {
+                  tasks: { $each: { project: true } },
+                  draftTasks: { $each: { project: true } },
+                  deletedTasks: { $each: { project: true } },
+                },
+              },
+            },
+          },
+        },
+      },
+      select: (account) =>
+        account?.root.organizations.flatMap((org) =>
+          org?.projects.flatMap((project) =>
+            project?.[taskListType]?.flatMap((task) => task),
+          ),
+        ),
+    });
+
+    return (
+      <div>
+        {subscription?.map((task) => (
+          <div key={task?.$jazz.id}>{task?.title}</div>
+        ))}
+      </div>
+    );
+  };
+
+  const MultipleSubscription = () => {
+    return (
+      <div>
+        <MultipleSubscriptionTasks taskListType="tasks" />
+        <MultipleSubscriptionTasks taskListType="draftTasks" />
+        <MultipleSubscriptionTasks taskListType="deletedTasks" />
+      </div>
+    );
+  };
+
+  bench(
+    "useAccountWithSelector",
+    () => {
+      render(<MultipleSubscription />);
+    },
+    { iterations: 200 },
   );
 });
