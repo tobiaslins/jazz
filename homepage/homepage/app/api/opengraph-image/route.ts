860ab3e118b76e3636a621c321e3669c8af8f623
Margaret Culotta <mculotta@Margarets-MacBook-Air.local>
2025-09-30 20:35:30 -0500

    update lock file and update og image text formatting

--- homepage/homepage/app/api/opengraph-image/route.ts
+++ homepage/homepage/app/api/opengraph-image/route.ts
@@ -6,9 +6,25 @@ import {
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const title = searchParams.get("title") ?? "Jazz";
-  const framework = searchParams.get("framework") ?? "";
-  const topic = searchParams.get("topic") ?? "";
-  const subtopic = searchParams.get("subtopic") ?? "";
+  const framework = searchParams.get("framework");
+  const topic = searchParams.get("topic");
+  const subtopic = searchParams.get("subtopic");
 
-  return OpenGraphImage({ title, framework, topic, subtopic });
+  const displayFramework =
+    framework && topic !== "server-side" ? formatLabel(framework) : "";
+
+  return OpenGraphImage({
+    title,
+    framework: displayFramework,
+    topic: formatLabel(topic),
+    subtopic: formatLabel(subtopic),
+  });
+}
+
+function formatLabel(str: string | null) {
+  if (!str) return "";
+  return str
+    .split("-")
+    .map((part) => part[0].toUpperCase() + part.slice(1))
+    .join(" ");
 }
