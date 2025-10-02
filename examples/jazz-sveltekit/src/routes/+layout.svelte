fa59a5fad211a07cf3f28c113f3476883d40650c
Joe Innes <joe@joeinn.es>
2025-10-02 14:20:29 +0200

    Integrate feedback

--- examples/jazz-sveltekit/src/routes/+layout.svelte
+++ examples/jazz-sveltekit/src/routes/+layout.svelte
@@ -6,6 +6,7 @@
   const sync = { peer: `wss://cloud.jazz.tools/?key=${apiKey}` };
 </script>
 
+<!-- Add the enableSSR flag to allow Jazz to render data server side -->
 <JazzSvelteProvider {sync} enableSSR>
   <main class="container" style="margin-top: 2rem;">
     {@render children?.()}
