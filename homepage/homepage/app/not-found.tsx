19580deeedf259029a4ac55d15ef218f65c7993c
Joe Innes <joe@joeinn.es>
2025-10-16 11:58:33 +0200

    feat: enhance NotFound page layout and functionality
    - Updated NotFound component to include JazzNav and JazzMobileNav for improved navigation.
    - Integrated DocProse for better content presentation.
    - Added HelpLinks component for additional resources.
    - Exported DocProse function for broader usage.

--- homepage/homepage/app/not-found.tsx
+++ homepage/homepage/app/not-found.tsx
@@ -1,13 +1,32 @@
+"use client";
+import { SideNavLayout } from "@/components/SideNavLayout";
+import { HelpLinks } from "@/components/docs/HelpLinks";
+import { JazzMobileNav } from "@/components/nav";
+import { DocProse } from "@/lib/docMdxContent";
 import { Icon404 } from "@garden-co/design-system/src/components/atoms/icons/404";
+import { usePathname } from "next/navigation";
+import { JazzNav } from "@/components/nav";
+
 
 export default function NotFound() {
   const text = "Don't Worry 'Bout Me";
+  const path = usePathname();
 
   return (
-    <div className="flex flex-col items-flex-start justify-center min-h-[95vh] w-auto pb-32">
-      <Icon404 className="w-75" />
-      <h1 className="text-3xl font-semibold my-7">{text}</h1>
-      <p>Page not found.</p>
-    </div>
+    <>
+      <div className="w-full">
+        <JazzNav />
+        <DocProse>
+          <h1>Couldn't find that page</h1>
+          <Icon404 className="w-[30%]" />
+        </DocProse>
+      </div>
+
+      <div className="pl-3 py-8 shrink-0 text-sm sticky align-start top-[61px] w-[16rem] h-[calc(100vh-61px)] overflow-y-auto hidden lg:block">
+        <HelpLinks />
+
+      </div>
+      <JazzMobileNav />
+    </>
   );
 }
