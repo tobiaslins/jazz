b3dbcaaf24d2c703ac144732e28abf5b52794c1e
Matteo Manchi <matteo.manchi@gmail.com>
2025-10-22 16:47:17 +0200

    feat(jazz-tools/inspector): Inspector's group view enhancements (#3086)

---
+++ packages/jazz-tools/src/inspector/ui/index.ts
@@ -0,0 +1,5 @@
+export { Button } from "./button.js";
+export { Icon } from "./icon.js";
+export { Modal } from "./modal.js";
+export { Input } from "./input.js";
+export { Select } from "./select.js";
