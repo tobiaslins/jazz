afff606070f0d598f3f71a6dae9d27c2b52a5d9b
Joe Innes <joe@joeinn.es>
2025-11-13 14:31:54 +0100

    Document pattern for creating set-like collections

--- homepage/homepage/content/docs/code-snippets/core-concepts/covalues/comaps/record.ts
+++ homepage/homepage/content/docs/code-snippets/core-concepts/covalues/comaps/record.ts
@@ -10,3 +10,56 @@ const inventory = Inventory.create({
 
 console.log(inventory["tomatoes"]); // 48
 // #endregion
+
+const Message = co.map({
+  content: z.string(),
+});
+const MyAppUser = co.account({
+  profile: co.profile({
+    avatar: co.image(),
+  }),
+  root: co.map({}),
+});
+const chatId = "";
+const me = await MyAppUser.load("");
+if (!me.$isLoaded) throw new Error();
+
+// #region RecordAsSet
+const Chat = co.map({
+  messages: co.list(Message),
+  participants: co.record(z.string(), MyAppUser),
+});
+
+const chat = await Chat.load(chatId, {
+  resolve: {
+    participants: true,
+  },
+});
+
+let participantList: string[];
+
+// Note that I don't need to load the map deeply to read and set keys
+if (chat.$isLoaded) {
+  chat.participants.$jazz.set(me.$jazz.id, me);
+  participantList = Object.keys(chat.participants);
+}
+// #endregion
+
+if (!chat.$isLoaded) throw new Error();
+// #region DeeplyLoadingKeys
+const { participants } = await chat.$jazz.ensureLoaded({
+  resolve: {
+    participants: {
+      $each: {
+        profile: {
+          avatar: true,
+        },
+      },
+    },
+  },
+});
+
+const avatarList = Object.values(participants).map(
+  (user) => user.profile.avatar,
+);
+// #endregion
